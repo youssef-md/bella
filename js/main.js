580f811887d0fa887f8bd240450fc0431e360ecd
youssef-md <emaildeyoussefmuhamad@gmail.com>
2020-12-06 18:59:30 -0300

    Animate header gallery in a 3d parallax effect

--- js/main.js
+++ js/main.js
@@ -35,13 +35,49 @@ function initNavigation() {
         onEnter: ({ direction }) => navAnimation(direction),
         onLeaveBack: ({ direction }) => navAnimation(direction)
     })
+}
 
-   
+function initHeaderTilt() {
+    document.querySelector('header').addEventListener('mousemove', moveImages)
+
+}
+
+function moveImages(e) {
+    const { offsetX, offsetY, target } = e
+    const { clientWidth, clientHeight } = target
+
+    // 0,0 in the center
+    const xPos = (offsetX / clientWidth) - 0.5
+    const yPos = (offsetY / clientHeight) - 0.5
+
+
+    const modifier = index => index * 1.8 * 0.5
+
+    const leftImages = gsap.utils.toArray('.hg__left .hg__image')
+    const rightImages = gsap.utils.toArray('.hg__right .hg__image')
+    
+
+    tiltTweenImages(leftImages, 1)
+    tiltTweenImages(rightImages, -1)
+    tiltTweenImages(['.decor__circle'], 1)
+
+    function tiltTweenImages(images, direction) {
+        return images.map((image, index) => {
+            gsap.to(image, {
+                duration: 0.8,
+                x: xPos * 20 * modifier(index),
+                y: direction * yPos * 30 * modifier(index),
+                rotationX: yPos * 10,
+                rotationY: xPos * 30,
+            })
+        })
+    }
 }
 
 function init(){
-    
+
     initNavigation()
+    initHeaderTilt()
 }
 
 window.addEventListener('load', function(){
