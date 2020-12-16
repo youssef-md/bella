74032a816c76e59e5381b0f46dd27a1faba95935
youssef-md <emaildeyoussefmuhamad@gmail.com>
2020-12-15 23:31:30 -0300

    Animate images and text in the portfolio section on hover

--- js/main.js
+++ js/main.js
@@ -129,15 +129,60 @@ function createHoverReveal(e) {
     return tl
 }
 
-// function init(){
 
-//     initNavigation()
-//     initHeaderTilt()
-// }
 
-// window.addEventListener('load', function(){
-//     init()
-// })
+function initPortfolioHover(){
+    const allLinks = gsap.utils.toArray('.portfolio__categories a')
+
+    allLinks.forEach(link => {
+        link.addEventListener('mouseenter', createPortfolioHover)
+        link.addEventListener('mouseleave', createPortfolioHover)
+    });
+}
+
+function createPortfolioHover(e) {
+    const allLinks = gsap.utils.toArray('.portfolio__categories a')
+    const pageBackground = document.querySelector('.fill-background')
+    const largeImage = document.querySelector('.portfolio__image--l')
+    const smallImage = document.querySelector('.portfolio__image--s')
+    const lInside = document.querySelector('.portfolio__image--l .image_inside')
+    const sInside = document.querySelector('.portfolio__image--s .image_inside')
+
+    if(e.type === 'mouseenter') {
+  
+        const { color, imagelarge, imagesmall } = e.target.dataset
+        const allSiblings = allLinks.filter(item => item != e.target)
+
+        const tl = gsap.timeline()
+        tl
+            .set(lInside, { backgroundImage: `url(${imagelarge})` })
+            .set(sInside, { backgroundImage: `url(${imagesmall})` })
+            .to([largeImage, smallImage], { duration: 1, autoAlpha: 1 })
+            .to(allSiblings, { color: '#fff', autoAlpha: 0.2 }, 0)
+            .to(e.target, { color: '#fff', autoAlpha: 1 }, 0)
+            .to(pageBackground, { backgroundColor: color, ease: 'none' }, 0)
+
+
+    } else if(e.type === 'mouseleave'){
+ 
+        // fade out images
+        // all links back to black
+        // change background color back to default #ACB7AE
+ 
+    }
+ 
+}
+ 
+function init(){
+
+    initNavigation()
+    initHeaderTilt()
+    initPortfolioHover()
+}
+
+window.addEventListener('load', function(){
+    init()
+})
 
 
 const mq = window.matchMedia('(max-width: 768px)')
